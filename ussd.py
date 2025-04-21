65d30ca716a9848e5ebbce0c9efb834f5d1f2b9f
GloryMsasalaga <glorymsasalaga@gmail.com>
2025-04-21 22:12:03 +0300

    update chatbot functionality and fixing bugs

---
+++ ussd.py
@@ -0,0 +1,8 @@
+
+    # print(bot.respond("Habari!"))
+    # while True:
+    #     msg = input("You: ")
+    #     resp = bot.respond(msg)
+    #     print(f"Bot: {resp}")
+    #     if any(b in msg.lower() for b in bot.farewells):
+    #         break
